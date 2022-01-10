fc7711b0eae832c19bb04464f431fc21ea0882a4
Christopher McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2022-01-10 22:15:22 +0000

    Assert row value equality by index

--- tests/test_pyodbc_compat.py
+++ tests/test_pyodbc_compat.py
@@ -5,6 +5,9 @@ from typing import List
 import pytest
 
 
+sql = "select * from information_schema.tables"
+
+
 def get_attrs(foo) -> List[str]:
     return sorted([attr for attr in dir(foo) if not attr.startswith("_")])
 
@@ -25,17 +28,17 @@ def test_cursor_attributes(cursor, pyodbc_cursor):
 
 
 def test_cursor_description(cursor, pyodbc_cursor):
-    sql = "select * from information_schema.tables"
     cursor.execute(sql)
     pyodbc_cursor.execute(sql)
     assert cursor.description == pyodbc_cursor.description
 
 
 def test_cursor_fetchone(cursor, pyodbc_cursor):
-    sql = "select * from information_schema.tables"
+
     a = cursor.execute(sql).fetchone()
     b = pyodbc_cursor.execute(sql).fetchone()
 
-    for col in b.cursor_description:
+    for i, col in enumerate(pyodbc_cursor.description):
         col_name = col[0]
         assert getattr(a, col_name) == getattr(b, col_name)
+        assert a[i] == b[i]
