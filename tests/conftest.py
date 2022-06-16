d334171a033b0ca9dc688ffaaaee68ce917d3d16
Christopher McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2022-06-16 23:45:21 +0100

    Skip tests via env vars

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import typing
 
@@ -11,22 +12,59 @@ PLATFORM = platform.system()
 
 @pytest.fixture(
     params=[
-        "DRIVER={ODBC Driver 17 for SQL Server};PORT=1433;",
-        "DRIVER={SQL Server};PORT=1433;",
-        "DRIVER={FreeTDS};PORT=1433;",
-        "DRIVER={PostgreSQL Unicode};PORT=5432;",
+        pytest.param(
+            ("ODBC Driver 17 for SQL Server", 1433),
+            id="msodbcsql17",
+            marks=[
+                pytest.mark.skipif(
+                    os.getenv("PUREPYODBC_SKIP_MSODBCSQL17", False),
+                    reason="Skipped via environment variable",
+                )
+            ],
+        ),
+        pytest.param(
+            ("SQL Server", 1433),
+            id="SQL Server (MDAC)",
+            marks=[
+                pytest.mark.skipif(
+                    os.getenv("PUREPYODBC_SKIP_MDAC", False),
+                    reason="Skipped via environment variable",
+                ),
+                pytest.mark.skipif(
+                    PLATFORM != "Windows",
+                    reason=f"MDAC Driver not supported on {PLATFORM}.",
+                ),
+            ],
+        ),
+        pytest.param(
+            ("FreeTDS", 1433),
+            id="FreeTDS",
+            marks=[
+                pytest.mark.skipif(
+                    os.getenv("PUREPYODBC_SKIP_FREETDS", False),
+                    reason="Skipped via environment variable",
+                ),
+                pytest.mark.skipif(
+                    PLATFORM == "Windows",
+                    reason="FreeTDS driver not supported on Windows.",
+                ),
+            ],
+        ),
+        pytest.param(
+            ("PostgreSQL Unicode", 5432),
+            id="PgSQL Unicode",
+            marks=[
+                pytest.mark.skipif(
+                    os.getenv("PUREPYODBC_SKIP_PGSQL_U", False),
+                    reason="Skipped via environment variable",
+                ),
+            ],
+        ),
     ]
 )
 def connection_string(request) -> str:
-    conn_str_fragment = request.param
-    if PLATFORM == "Linux":
-        if "{SQL Server}" in conn_str_fragment:
-            pytest.skip("MDAC SQL Server Driver not supported on Linux.")
-    elif PLATFORM == "Windows":
-        if "{FreeTDS}" in conn_str_fragment:
-            pytest.skip("FreeTDS not supported on Windows")
-
-    return conn_str_fragment + "SERVER=localhost;UID=sa;PWD=Password123;"
+    driver, port = request.param
+    return f"DRIVER={{{driver}}};PORT={port};SERVER=localhost;UID=sa;PWD=Password123;"
 
 
 @pytest.fixture
